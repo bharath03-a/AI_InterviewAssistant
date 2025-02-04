d87e48dc33660d812dac8419871ff90995accc8b
Bharath Velamala <bharathvelamala1929@gmail.com>
2025-02-03 20:55:35 -0700

    front end set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <div>
+        <div className='text-2xl bg-blue-500'> Hello</div>
+      </div>
+  )
+}
+
+export default App
