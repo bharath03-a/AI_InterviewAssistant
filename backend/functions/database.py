90bb972c8b87d0694de32e53928ab57ec96fd3d1
Bharath Velamala <bharathvelamala1929@gmail.com>
2025-02-03 20:56:55 -0700

    backend functions

---
+++ backend/functions/database.py
@@ -0,0 +1,49 @@
+import os
+import json
+import random
+
+# Get recent messages
+def get_recent_messages():
+
+    # defining parameters
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_name = os.path.join(base_dir, 'files', 'db', 'stored_data.json')
+    learn_instructions = {
+        "role": "system",
+        "content": "You are interviewing the user for a job as a retail assistant. Ask short questions that are relevant to the junior position. Your name is Rachel and the use is called Bharath. Keep your answers to under 30 words."
+    }
+
+    # Initialize messages
+    messages = []
+
+    # add randomness
+    x = random.uniform(0, 1)
+    if x < 0.5:
+        learn_instructions["content"] = learn_instructions["content"] + " Your response will include some dry humour."
+    else:
+        learn_instructions["content"] = learn_instructions["content"] + " Your response will include a rather challenging question."
+
+    # Append instructions to message
+    messages.append(learn_instructions)
+
+    # getting history of messages
+    try:
+        if os.path.getsize(file_name) > 0:
+            with open(file_name, "r") as file:
+                data = json.load(file)
+                # appending last 5 messages
+                if data:
+                    if len(data) < 5:
+                        for item in data:
+                            messages.append(item)
+                    else:
+                        for item in data[-5:]:
+                            messages.append(item)
+    except Exception as e:
+        print(f"Exception has occured: {e}")
+        pass
+
+    return messages
+
+def store_messages():
+    pass
---
+++ backend/functions/llm_requests.py
@@ -0,0 +1,36 @@
+import whisper
+import ollama
+
+from functions.database import get_recent_messages
+
+# Convert Audio to Text
+def convert_audio_to_text(audio_file):
+    try:
+        model = whisper.load_model("base")
+        transcript = model.transcribe(audio_file)
+
+        return transcript["text"]
+    except Exception as e:
+        print(f"Exception has occured: {e}")
+        return
+
+# llama3.2b model - Get response from the model
+def get_chat_response(message_input):
+    messages = get_recent_messages()
+    user_message = {"role": "user", "content": message_input}
+
+    messages.append(user_message)
+    print(messages)
+
+    try:
+        response = ollama.chat(
+            model="llama3.2:latest",
+            messages=messages,
+        )
+        print("=======")
+        print(response)
+        message_text = response["choices"][0]["messages"]["content"]
+        return message_text
+    except Exception as e:
+        print(f"Exception has occured: {e}")
+        return
